dcb6b110429e6c5867043f78e5efa3bc9513a89f
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-06-28 07:39:55 -0400

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Taylor Patterson on 6/28/20.
+//  Copyright © 2020 Taylor Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
