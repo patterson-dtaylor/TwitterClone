73c9042e495946f3ed5d00ed57f28bbed45b6667
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-07-12 07:52:23 -0400

    Search UI completed in ExploreContoller

--- TwitterClone/API/UserService.swift
+++ TwitterClone/API/UserService.swift
@@ -23,4 +23,20 @@ struct UserService {
             completion(user)
         }
     }
+    
+    func fetchUsers(completion: @escaping([User]) -> Void) {
+        
+        var users = [User]()
+        
+        REF_USERS.observe(.childAdded) { snapshot in
+            let uid = snapshot.key
+            guard let dicitonary = snapshot.value as? [String: AnyObject] else { return }
+            
+            let user = User(uid: uid, dictionary: dicitonary)
+            
+            users.append(user)
+            
+            completion(users)
+        }
+    }
 }
