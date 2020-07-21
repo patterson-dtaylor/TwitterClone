6bc803f437cbf46126e7b98b51eaef551c5c0269
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-07-21 08:16:27 -0400

    updated user feed sorted timeline

--- TwitterClone/API/TweetService.swift
+++ TwitterClone/API/TweetService.swift
@@ -44,15 +44,25 @@ struct TweetService {
     
     func fetchTweets(completion: @escaping([Tweet]) -> Void) {
         var tweets = [Tweet]()
+        guard let currentUID = Auth.auth().currentUser?.uid else { return }
         
-        REF_TWEETS.observe(.childAdded) { snapshot in
-            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
-            guard let uid = dictionary["uid"] as? String else { return }
+        REF_USER_FOLLOWING.child(currentUID).observe(.childAdded) { snapshot in
+            let followingUID = snapshot.key
             
+            REF_USER_TWEETS.child(followingUID).observe(.childAdded) { snapshot in
+                let tweetID = snapshot.key
+                
+                self.fetchTweet(withTweetID: tweetID) { tweet in
+                    tweets.append(tweet)
+                    completion(tweets)
+                }
+            }
+        }
+        
+        REF_USER_TWEETS.child(currentUID).observe(.childAdded) { snapshot in
             let tweetID = snapshot.key
             
-            UserService.shared.fetchUser(uid: uid) { user in
-                let tweet = Tweet(user: user, tweetID: tweetID, dictionary: dictionary)
+            self.fetchTweet(withTweetID: tweetID) { tweet in
                 tweets.append(tweet)
                 completion(tweets)
             }
